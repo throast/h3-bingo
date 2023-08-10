6ec2bd5c94b2e0eb4e66807c6225ea394073f642
throast <141887667+throast@users.noreply.github.com>
2023-08-10 17:55:32 +0000

    add layout sections

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,26 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
 </script>
 
+<header>
+    <h1>H3 Bingo</h1>
+    <!-- Create Nav Component -->
+    <nav>
+        <button>Menu</button>
+    </nav>
+</header>
+
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1 class="text-green-500">Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <section id="bingo-card" />
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<!-- Create Nav Component -->
+<nav>
+    <button>Menu</button>
+</nav>
+
+<footer>
+    <a href="https://github.com/throast/h3-bingo">Code available on GitHub</a>
+    <a href="#stripe-checkout-unsure">
+        Buy me some Gamer Supps or Teddy Fresh &lt;3
+    </a>
+</footer>
